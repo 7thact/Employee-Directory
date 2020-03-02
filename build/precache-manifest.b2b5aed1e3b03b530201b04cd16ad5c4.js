97c1eb6fe1d16d9e17df317d54e0c39adf947087
Charles Cressey <cmcressey@gmail.com>
2020-03-01 23:37:44 -0700

    updated files

---
+++ build/precache-manifest.b2b5aed1e3b03b530201b04cd16ad5c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdf4bdd35d759bb6b61a2956eb130d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d34ac92d7975967eff",
+    "url": "/static/css/main.e003cb04.chunk.css"
+  },
+  {
+    "revision": "da9136ca49e2a87593a1",
+    "url": "/static/js/2.001c12f6.chunk.js"
+  },
+  {
+    "revision": "05d34ac92d7975967eff",
+    "url": "/static/js/main.4057e8f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
